0feb942cf99d775b9ab1c7ab7c20ef61a4be235b
aleynayesil <aleyna.yesil@freightest.com>
2024-08-22 14:34:41 +0300

    added atomic design

--- social-media-management-app/src/app/app.module.ts
+++ social-media-management-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { SocialMediaListComponent } from './modules/home/social-media-list/social-media-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthComponent,
+    NavbarComponent,
+    SocialMediaListComponent
   ],
   imports: [
     BrowserModule,
